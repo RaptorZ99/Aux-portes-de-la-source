e6a8b0d8555e1f9236550fc3f12420cc9a8721d8
RaptorZ99 <loulou.scarfone@gmail.com>
2023-10-23 19:50:51 +0200

    Maj 90% Navbar hide&appear

--- navbar-scroll.js
+++ navbar-scroll.js
@@ -1,22 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.querySelector('.header-navbar');
-    const buttons = navbar.querySelectorAll('button');
-
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 0) {
-            buttons.forEach(button => {
-                button.classList.add("small-text");
-            });
-        } else {
-            buttons.forEach(button => {
-                button.classList.remove("small-text");
-            });
-        }
-    });
-    
-});
-
-
+// Fonction pour naviguer vers la section
 function scrollToSection(id) {
     var elem = document.getElementById(id);
     var elemHeight = elem.offsetHeight;
@@ -30,3 +12,56 @@ function scrollToSection(id) {
         behavior: 'smooth'
     });
 }
+
+// Gestion de la navbar
+const navbar = document.querySelector(".header-navbar");
+const logoButton = document.getElementById("section0");
+const leftButtons = navbar.querySelectorAll(":nth-child(-n+3)");
+const rightButtons = navbar.querySelectorAll(":nth-child(n+5)");
+
+function showButtons() {
+    leftButtons.forEach(btn => {
+        btn.style.transform = "translateX(0) scaleX(1)";
+        btn.style.opacity = "1";
+    });
+    rightButtons.forEach(btn => {
+        btn.style.transform = "translateX(0) scaleX(1)";
+        btn.style.opacity = "1";
+    });
+}
+
+function hideButtons() {
+    leftButtons.forEach(btn => {
+        btn.style.transform = "translateX(100%) scaleX(0)";
+        btn.style.opacity = "0";
+    });
+    rightButtons.forEach(btn => {
+        btn.style.transform = "translateX(-100%) scaleX(0)";
+        btn.style.opacity = "0";
+    });
+}
+
+navbar.addEventListener("mouseleave", function() {
+    if (window.scrollY > window.innerHeight) {
+        hideButtons();
+    }
+});
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY <= window.innerHeight) {
+        showButtons();
+    } else {
+        hideButtons();
+    }
+});
+
+logoButton.addEventListener("mouseover", function() {
+    if (window.scrollY > window.innerHeight) {
+        showButtons();
+    }
+});
+
+// Écouteur d'événements pour le clic sur le logo
+document.querySelector("#section0").addEventListener("click", function() {
+    window.location.href = "#"; // Cette ligne naviguera vers le haut de la page
+});
